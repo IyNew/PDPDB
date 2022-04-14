b50158c210e0f708df3cb121e9d306523a627fd6
megadonny <33389261+megadonny@users.noreply.github.com>
2022-04-14 00:07:21 -0400

    optimized

--- EVC/timing.sh
+++ EVC/timing.sh
@@ -3,16 +3,16 @@
 
 gcc evc.c -o timing.o -lgmp -lpbc
 
-echo "n,k,size_of_operation,CommitSet,MemberP,MemberV,UpdateCommitment,AggP,AggV,EqualP,EqualV*k" >> timing.csv
+echo "n,k,size_T,CommitSet (s),MemberProve (s),MemberVerify (s),UpdateCommit (s),AggProof (s),AggVerify (s),EqualProve (s),EqualVerify (s)" >> timing.csv
 
-for i in 10 20 40 80 160 320 640 1280 2560 5120
+for i in 40 80 160 320 640 1280 2560 5120 10240 20480 40960 81920
 do
-    echo "Timing the case when k = $i and size_of_operation = 10"
-	./timing.o $i 10 >> timing.csv
+	echo "Timing the case when k = $i and size_T = 40"
+	./timing.o $i 40 >> timing.csv
 done
 
-for i in 5 10 20 40 80 160 320 640
+for i in 40 80 160 320 640 1280 2560 5120 10240
 do
-    echo "Timing the case when k = 640 and size_of_operation = $i"
-	./timing.o 640 $i >> timing.csv
+    echo "Timing the case when k = 81920 and size_T = $i"
+	./timing.o 81920 $i >> timing.csv
 done
